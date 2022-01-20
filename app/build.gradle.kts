a8a34604802c46a282e4c18f04064fb7b85988cc
Amr Saraya <amrmsaraya@gmail.com>
2022-01-20 16:18:19 +0200

    - Use Material3 "Material You" theme - Implement Splashscreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,10 @@ android {
     kotlinOptions {
         jvmTarget = AndroidConfig.jvmTarget
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
--- app/src/main/java/com/github/amrmsaraya/expirytracker/MainActivity.kt
+++ app/src/main/java/com/github/amrmsaraya/expirytracker/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.github.amrmsaraya.expirytracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContentView(R.layout.activity_main)
 
     }
