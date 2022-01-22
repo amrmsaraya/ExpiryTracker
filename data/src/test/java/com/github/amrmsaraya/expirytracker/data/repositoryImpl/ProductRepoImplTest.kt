a63ab85241ed5c50a446e9be68336954e8637c73
Amr Saraya <amrmsaraya@gmail.com>
2022-01-22 19:53:38 +0200

    Unit Test: ProductRepo

--- data/src/test/java/com/github/amrmsaraya/expirytracker/data/repositoryImpl/ProductRepoImplTest.kt
+++ data/src/test/java/com/github/amrmsaraya/expirytracker/data/repositoryImpl/ProductRepoImplTest.kt
@@ -1,8 +1,8 @@
-package com.github.amrmsaraya.expirytracker.data.sourceImpl
+package com.github.amrmsaraya.expirytracker.data.repositoryImpl
 
-import com.github.amrmsaraya.expirytracker.data.local.ProductDao
 import com.github.amrmsaraya.expirytracker.data.model.ProductDTO
 import com.github.amrmsaraya.expirytracker.data.source.LocalDataSource
+import com.github.amrmsaraya.expirytracker.domain.repository.ProductRepo
 import com.google.common.truth.Truth.assertThat
 import io.mockk.coVerify
 import io.mockk.every
@@ -10,24 +10,30 @@ import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
-class LocalDataSourceImplTest {
+class ProductRepoImplTest {
 
-    private lateinit var productDao: ProductDao
     private lateinit var localDataSource: LocalDataSource
+    private lateinit var productRepo: ProductRepo
+
+    private val standardTestDispatcher = StandardTestDispatcher()
 
     @Before
     fun setUp() {
-        productDao = mockk(relaxed = true)
-        localDataSource = LocalDataSourceImpl(productDao)
+        localDataSource = mockk(relaxed = true)
+        productRepo = ProductRepoImpl(
+            localDataSource = localDataSource,
+            dispatcher = standardTestDispatcher
+        )
     }
 
     @Test
-    fun `insert() with product then insert() in ProductDao() should be called`() = runTest {
+    fun `insert() with product then insert() in LocalDataSource() should be called`() = runTest {
 
         // Given
         val product = ProductDTO()
@@ -36,11 +42,11 @@ class LocalDataSourceImplTest {
         localDataSource.insert(product)
 
         // Then
-        coVerify { productDao.insert(product) }
+        coVerify { localDataSource.insert(product) }
     }
 
     @Test
-    fun `delete() with product then delete() in ProductDao() should be called`() = runTest {
+    fun `delete() with product then delete() in LocalDataSource() should be called`() = runTest {
 
         // Given
         val product = ProductDTO()
@@ -49,7 +55,7 @@ class LocalDataSourceImplTest {
         localDataSource.delete(product)
 
         // Then
-        coVerify { productDao.delete(product) }
+        coVerify { localDataSource.delete(product) }
     }
 
     @Test
@@ -59,7 +65,7 @@ class LocalDataSourceImplTest {
             // Given
             val product1 = ProductDTO(barcode = "12345")
             val product2 = ProductDTO(barcode = "abcdef")
-            every { productDao.getProducts() } returns flowOf(listOf(product1, product2))
+            every { localDataSource.getProducts() } returns flowOf(listOf(product1, product2))
 
             // When
             val result = localDataSource.getProducts().first()
